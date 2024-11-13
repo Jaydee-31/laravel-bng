0161072ad965ce2337c5cbc23fd4bc87453cb33e
Jaydee-31 <johndextergamo@gmail.com>
2024-11-14 03:31:58 +0800

    added countries
    Signed-off-by: Jaydee-31 <johndextergamo@gmail.com>

--- resources/views/navigation-menu.blade.php
+++ resources/views/navigation-menu.blade.php
@@ -15,6 +15,9 @@
                     <x-nav-link href="{{ route('dashboard') }}" :active="request()->routeIs('dashboard')">
                         {{ __('Dashboard') }}
                     </x-nav-link>
+                    <x-nav-link href="{{ route('countries') }}" :active="request()->routeIs('countries')">
+                        {{ __('Countries') }}
+                    </x-nav-link>
                     <x-nav-link href="{{ route('vendors') }}" :active="request()->routeIs('vendors')">
                         {{ __('Vendors') }}
                     </x-nav-link>
@@ -147,6 +150,9 @@
         <div class="pt-2 pb-3 space-y-1">
             <x-responsive-nav-link href="{{ route('dashboard') }}" :active="request()->routeIs('dashboard')">
                 {{ __('Dashboard') }}
+            </x-responsive-nav-link>
+            <x-responsive-nav-link href="{{ route('countries') }}" :active="request()->routeIs('countries')">
+                {{ __('Countries') }}
             </x-responsive-nav-link>
             <x-responsive-nav-link href="{{ route('vendors') }}" :active="request()->routeIs('vendors')">
                 {{ __('Vendors') }}
