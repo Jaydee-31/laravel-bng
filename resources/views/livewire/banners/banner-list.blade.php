c20ea462714ce33890c1a931782a349401cf5e68
Jaydee-31 <johndextergamo@gmail.com>
2024-11-19 05:37:13 +0800

    added edit & delete button

--- resources/views/livewire/banners/banner-list.blade.php
+++ resources/views/livewire/banners/banner-list.blade.php
@@ -9,14 +9,14 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="col-md-8 mb-2">
                 @if(session()->has('success'))
-                    <x-alert>
-                        {{ session()->get('success') }}
-                    </x-alert>
+                <x-alert>
+                    {{ session()->get('success') }}
+                </x-alert>
                 @endif
                 @if(session()->has('error'))
-                    <x-alert>
-                        {{ session()->get('error') }}
-                    </x-alert>
+                <x-alert>
+                    {{ session()->get('error') }}
+                </x-alert>
                 @endif
             </div>
 
@@ -24,7 +24,7 @@
                 <x-button wire:click="openModal" wire:loading.attr="disabled">
                     {{ __('Add') }}
                 </x-button>
-                <x-input wire:model.live="search" type="text" placeholder="Search vendors.."/>
+                <x-input wire:model.live="search" type="text" placeholder="Search vendors.." />
             </div>
 
             @include('livewire.banners.banner-form')
@@ -54,34 +54,35 @@
 
                                 <tbody class="bg-white dark:bg-gray-800 dark:bg-opacity-50 divide-y divide-gray-200 dark:divide-gray-700">
                                     @forelse($banners as $banner)
-                                        <tr>
-                                            <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-900 dark:text-white">
-                                                {{ $banner->id }}
-                                            </td>
+                                    <tr>
+                                        <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-900 dark:text-white">
+                                            {{ $banner->id }}
+                                        </td>
 
-                                            <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-900 dark:text-white max-w-xs overflow-ellipsis truncate">
-                                                {{ $banner->name }}
-                                            </td>
+                                        <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-900 dark:text-white max-w-xs overflow-ellipsis truncate">
+                                            {{ $banner->name }}
+                                        </td>
 
-                                            <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-900 dark:text-white max-w-xs overflow-ellipsis truncate">
-                                                {{ implode(', ', $banner->sizes) }}
-                                            </td>
+                                        <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-900 dark:text-white max-w-xs overflow-ellipsis truncate">
+                                            {{ implode(', ', $banner->sizes) }}
+                                        </td>
 
-                                            <td class="px-6 py-4 whitespace-nowrap text-sm font-medium">
-                                                <button x-on:click="$wire.openModal({{ $banner->id }})" class="text-blue-600 dark:text-blue-500 hover:text-blue-900 mr-4 ">Edit</button>
-                                                <button wire:click="delete({{ $banner->id }})"
-                                                        wire:confirm="Are you sure you want to delete this record?" type="button" class="text-red-600 dark:text-red-400 hover:text-red-900 cursor-pointer mr-2"
-                                                >
-                                                    Delete
-                                                </button>
-                                            </td>
-                                        </tr>
+                                        <td class="px-6 py-4 whitespace-nowrap text-sm font-medium">
+                                            <x-edit-button x-on:click="$wire.openModal({{ $banner->id }})" class="mr-4 ">
+                                                {{ __('Edit') }}
+                                            </x-edit-button>
+                                            <x-delete-button wire:click=" delete({{ $banner->id }})"
+                                                wire:confirm="Are you sure you want to delete this record?" type="button" class="mr-2">
+                                                Delete
+                                            </x-delete-button>
+                                        </td>
+                                    </tr>
                                     @empty
-                                        <tr>
-                                            <td colspan="4" class="px-6 py-4 dark:text-white text-sm leading-5 text-gray-900 whitespace-no-wrap">
-                                                No banners Found.
-                                            </td>
-                                        </tr>
+                                    <tr>
+                                        <td colspan="4" class="px-6 py-4 dark:text-white text-sm leading-5 text-gray-900 whitespace-no-wrap">
+                                            No banners Found.
+                                        </td>
+                                    </tr>
                                     @endforelse
                                 </tbody>
                             </table>
